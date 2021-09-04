1488e8ef2f2ca1bd787b6923178baa70c38157a8
elee <eddy@gfxlabs.io>
2021-09-04 17:35:27 -0500

    styling

--- contracts/District.sol
+++ contracts/District.sol
@@ -4,14 +4,9 @@ pragma solidity ^0.8.0;
 import "./openzeppelin/access/OwnableUpgradeable.sol";
 import "./openzeppelin/token/ERC721/ERC721Upgradeable.sol";
 
-
-
 import "./IDistrict.sol";
 
-
 contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
-
-
     /*** variables ***/
 
     // plot price is calculated through comparing the smaller of the absolute value of both coordinates
@@ -29,7 +24,6 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
     bool public claimable; // whether or not it is possible to mint an district or claim deeds
 
     uint128 public worldSize; // maximum value of any plot coordinate
-
 
     /*** mappings ***/
 
@@ -60,7 +54,6 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
     ) external override onlyOwner {
         require(_prices.length == _distances.length,
                 "District: Length doesn't match");
-
         plotPrices = _prices;
         plotPriceDistances = _distances;
     }
@@ -80,17 +73,19 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
     // the smallest subdivision of land is a plot
     // one plot represents a 16x16 plot of land within the minecraft game
 
-    function _calculateLandCost(int128 _x, int128 _z) internal view returns (uint256 price){
+    function _calculateLandCost(int128 _x, int128 _z) internal view returns (price){
         uint128 xA = uint128(_x >= 0 ? _x : -_x);
         uint128 zA = uint128(_z >= 0 ? _z : -_z);
         uint128 min = (xA < zA ? xA : zA);
 
-        price = 0;
+        uint256 price = 0;
         for (uint256 i = 0; i < plotPrices.length; i++) {
             if (min <= plotPriceDistances[i] && price < plotPrices[i]) {
                 price = plotPrices[i];
             }
         }
+
+        return price;
     }
 
     function transferPlot(uint256 origin_id, uint256 target_id, uint256[] calldata plot_ids) override external {
@@ -114,9 +109,6 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
     function claimDistrictLands(int128[] calldata _xs, int128[] calldata _zs, uint256 _districtId) external override payable {
         require(claimable,
                 "claiming is currently disabled");
-        require(_xs.length == _zs.length,
-            "xs and zs array lengths must match!");
-
         uint256 _id = _districtId;
         if(_districtId == 0){
             totalSupply = totalSupply + 1;
@@ -126,6 +118,8 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
             require(ERC721Upgradeable.ownerOf(_districtId) != address(0),
                     "District: Attempting to claim lands to nonexistent district");
         }
+        require(_xs.length == _zs.length,
+            "xs and zs array lengths must match!");
         // calculate total cost
         uint256 total_cost = 0;
         for (uint256 i = 0; i < _xs.length; i++) {
@@ -157,26 +151,20 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
         int128 _x,
         int128 _z
     ) internal returns (uint256 tokenId) {
-        require(
-            plotIdOf[_x][_z] == 0,
-            "attempting to claim already minted land"
-        );
+        require(plotIdOf[_x][_z] == 0,
+            "attempting to claim already minted land");
 
         uint128 xA = uint128(_x >= 0 ? _x : -_x);
         uint128 zA = uint128(_z >= 0 ? _z : -_z);
-        require(
-            (worldSize > xA) && (worldSize > zA),
-            "the claim is beyond the specified world size"
-        );
-
-        // tokenId starts from 1
+        require((worldSize > xA) && (worldSize > zA),
+            "the claim is beyond the specified world size");
+        // the first plot has id = 1;
         totalPlots = totalPlots + 1;
         plot_x[totalPlots] = _x;
         plot_z[totalPlots] = _z;
         plotIdOf[_x][_z] = totalPlots;
+        // a transfer from 0 indicates a mint;
         _transferPlot(0,_districtId,totalPlots);
-
-
         return totalPlots;
     }
 
@@ -184,10 +172,8 @@ contract District is ERC721Upgradeable, OwnableUpgradeable, IDistrict {
 
     // the owner of the contract may claim any unclaimed plot and assign it to any district id
     function adminClaim(int128[] calldata _xs, int128[] calldata _zs, uint256 _districtId) external override onlyOwner {
-        require(
-            _xs.length == _zs.length,
-            "xs and zs array lengths must match!"
-        );
+        require(_xs.length == _zs.length,
+            "xs and zs array lengths must match!");
         for (uint256 i = 0; i < _xs.length; i++) {
             _claimPlot(_districtId, _xs[i], _zs[i]);
         }
